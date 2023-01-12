c6d9f085e38c0d4b7d7d084b286d879022817da7
ysaias perez campos <ysaiascampos@gmail.com>
2023-01-12 09:52:22 +0100

    crear base de datos y tabla usuarios

---
+++ database/migrations/2023_01_12_083603_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 45);
+            $table->string('email',125)->unique();
+            $table->string('city', 125);
+            $table->string('password',125);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
